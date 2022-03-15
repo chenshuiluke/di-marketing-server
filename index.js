e0731ac8fbf2b4c4fbe53e6fafbf0926239df071
Carter Childs <carterchilds@live.com>
2022-03-15 15:59:13 -0600

    sf test

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const port = 8200;
+let jsforce = require("jsforce");
+app.use(express.json());
 
-app.use(express.json())
+var conn = new jsforce.Connection({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+  loginUrl: "https://login.salesforce.com/",
+});
+
+conn.login("cchilds@dentalintel.com", "iMoved2005!", function (err, userInfo) {
+  if (err) {
+    return console.error(err);
+  }
+  // Now you can get the access token and instance URL information.
+  // Save them to establish connection next time.
+  console.log(conn.accessToken);
+  console.log(conn.instanceUrl);
+  // logged in user property
+  console.log("User ID: " + userInfo.id);
+  console.log("Org ID: " + userInfo.organizationId);
+  // ...
+});
 
 app.get("/api/test", (req, res) => {
-    res.status(200).send("Working")
-})
+  res.status(200).send("Working");
+});
 
 app.get("/api/test2", (req, res) => {
-    res.status(200).send("Working two")
-})
+  res.status(200).send("Working two");
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`));
