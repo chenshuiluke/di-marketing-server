04b37e403f17b84cefdc035076ee87eaa36b0d3e
Carter Childs <carterchilds@live.com>
2022-04-05 15:16:03 -0600

    google sheet add

--- controllers/url-builder.js
+++ controllers/url-builder.js
@@ -1,7 +1,21 @@
 require("dotenv").config();
 const axios = require("axios");
+const { google } = require("googleapis");
+
 module.exports = {
   createMarketingUrl: async (req, res) => {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+
+    const client = await auth.getClient();
+    const googleSheets = google.sheets({
+      version: "v4",
+      auth: client,
+    });
+    const spreadsheetId = process.env.SPREADSHEET_ID;
+
     const headers = {
       "Content-Type": "application/json",
       apikey: process.env.REBRAND_API_KEY,
@@ -23,11 +37,20 @@ module.exports = {
     try {
       let apiResponse = await axios(apiCall);
       let link = apiResponse.data;
+
+      await googleSheets.spreadsheets.values.append({
+        auth,
+        spreadsheetId,
+        range: "url_list!A:D",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+          values: [[req.body.campaignName, link.shortUrl, req.body.destinationUrl, req.body.utmContent]],
+        },
+      });
       res.status(200).send(link.shortUrl);
-      console.log(link.shortUrl)
+      console.log(link.shortUrl);
     } catch (err) {
-      res.status(500).send(err);
-      console.log(err)
+      res.status(403).send(err);
     }
   },
 };
--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 const express = require("express");
 const app = express();
 const port = 8200;
-let jsforce = require("jsforce");
+const jsforce = require("jsforce");
 const urlBuilder = require("./controllers/url-builder");
 app.use(express.json());
 app.use(cors())
+
+
+ 
 
 var conn = new jsforce.Connection({
   // you can change loginUrl to connect to sandbox or prerelease env.
