b45821428c2aed67d167a34db87bc0b585688914
Carter Childs <carterchilds@live.com>
2022-05-05 11:09:06 -0600

    get to post

--- index.js
+++ index.js
@@ -54,4 +54,4 @@ app.get("/api/getModentoPartners", partnerCreation.getModentoPartners);
 app.get("/api/getLinks", urlBuilder.getAllLinks)
 
 // CUSTOMER REFERRAL ROUTES
-app.get("/api/createReferralLink", referral.createReferralLink)
+app.post("/api/createReferralLink", referral.createReferralLink)
