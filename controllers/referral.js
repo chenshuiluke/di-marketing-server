3229bc171ad3c6cf92ce526c7fb033382c552e91
Carter Childs <carterchilds@live.com>
2022-05-10 11:33:22 -0600

    ranodm num change

--- controllers/referral.js
+++ controllers/referral.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 module.exports = {
   createReferralLink: async (req, res) => {
-    var num = Math.floor(1000 + Math.random() * 9000);
+    var num = Math.floor(Math.random() * 90 + 10)
     const { utmCampaign, campaignId, slashTag, accountId } = req.query;
 
     let destinationUrl = `https://get.dentalintel.com/watch/?campaign_id=${campaignId}&utm_campaign=${utmCampaign}&utm_source=customer_referrral&account_id=${accountId}`;
